f5dc23c1cfd891338fc585eb97131732e32b82f4
alexgplay <alexgp1234@gmail.com>
2021-08-02 18:55:42 +0200

    Added layout for url shortener

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,10 +1,37 @@
-import { Input } from "@chakra-ui/react";
+import styles from "../styles/Main.module.css";
+
+import * as React from "react";
+import { Input, Flex, Heading, Box, Text, useColorMode } from "@chakra-ui/react";
 
 export default function Main() {
+  const [text, setText] = React.useState("");
+  const [textAsLabel, setTextAsLabel] = React.useState(false);
+  const { colorMode } = useColorMode();
+
+  const handleTextChange = React.useCallback((evt) => setText(evt.target.value), []);
+
   return (
-    <h1>
-      Test
-      <Input />
-    </h1>
+    <main className={styles.main}>
+      <Flex direction="column" width={780} className={styles.container}>
+        <Flex justify="center" marginBottom={10}>
+          <Heading size="2xl">URL Shortener</Heading>
+        </Flex>
+        <Box className={styles.inputContainer}>
+          <Input
+            value={text}
+            onChange={handleTextChange}
+            onFocus={() => setTextAsLabel(true)}
+            onBlur={() => setTextAsLabel(text.length > 0)}
+          />
+          <Text
+            className={`${styles.inputText} ${textAsLabel ? styles.label : ""} ${
+              styles[colorMode]
+            }`}
+          >
+            URL
+          </Text>
+        </Box>
+      </Flex>
+    </main>
   );
 }
