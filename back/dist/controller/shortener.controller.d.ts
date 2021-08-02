a7d2f1f7f322631ca49e4f150bac8c6fe831aba9
alexgplay <alexgp1234@gmail.com>
2021-08-03 00:39:55 +0200

    Removed dist and added to gitignore

--- back/dist/controller/shortener.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ShortenerService } from './../services/shortener.service';
-export declare class ShortenerController {
-    private readonly shortenerService;
-    constructor(shortenerService: ShortenerService);
-    root(): string;
-    obtainUrl(slug: string): Promise<Object>;
-    shortenUrl(body: any): Promise<Object>;
-}
--- back/dist/services/shortener.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from './prisma.service';
-export declare class ShortenerService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    createUniqueUrl(url: string): Promise<string>;
-    getUrlFromSlug(slug: string): Promise<string>;
-    private generateSlug;
-}
